cfaa9ca7b6fcca55d270e693b3f5fff780d1c317
yshzjq <ysh10150@gmail.com>
2024-10-16 19:23:04 +0900

    asd

---
+++ Assets/testscript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    void Start()
+    {
+        text.text = "hello";
+
+    }
+
+}
